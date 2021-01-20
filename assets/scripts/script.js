83639916b73d47eb8060699802ac0f1a37fe548c
Juan-CamiloF <jucamilogac@outlook.com>
2021-01-20 15:36:03 -0500

    Calculadora avanzada con historial

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,70 +1,115 @@
-const tecla = document.querySelectorAll('.tecla')
-const barra = document.getElementById('barra');
-let operador
-//Teclado de la calculadora
+//Declaración de variables
+const tecla = document.querySelectorAll(".tecla");
+const barra = document.getElementById("barra");
+const borrar = document.getElementById("borrar");
+const borrarH = document.getElementById("borrarHistorial");
+
+let operador;
+let numero1, numero2;
+
+//Función del teclado
 const teclado = (event) => {
-    const texto = event.target.innerText
-    if (texto == '<--') {
-        barra.innerHTML = barra.innerText.slice(0, -1)
+  const texto = event.target.innerText;
+  //Capto el operador
+  if (texto == "+" || texto == "-" || texto == "x" || texto == "/") {
+    if(!operador){
+        operador = texto;
+        console.log("operador", operador);
+        if (barra != "") {
+        // barra.innerText = operador;
+        numero1 = parseInt(barra.innerText);
+        barra.innerText = texto;
+        console.log("numero1", numero1);
+        } 
+    }
+    if(operador==undefined){
+        console.log('ijole')
+        operador = texto;
+    }else {
+        barra.innerText = operador;
+        }
+    
+  } else {
+    if (texto == "←") {
+      barra.innerHTML = barra.innerText.slice(0, -1);
     } else {
-        barra.innerText += texto
-    }if(texto == '+' || texto == '-' || texto == 'x' ||texto=='/'){
-        operador = texto
+      hacerTexto(texto);
     }
-    if (texto == '=') {
-        operar(barra.innerHTML,operador)
-        barra.innerHTML = ''
+  }
+  //Capto los números
+  if (Number.isInteger(parseInt(numero1)) && operador) {
+    if (
+      barra.innerText == "+" ||
+      barra.innerText == "-" ||
+      barra.innerText == "x" ||
+      barra.innerText == "/"
+    ) {
+      barra.innerText = "";
     }
-}
+    if (texto == "=") {
+      numero2 = barra.innerText;
+      barra.innerText = "";
+      console.log(numero2);
+      calcular(numero1, numero2, operador, barra);
+      numero2 = 0;
+      operador = '';
+    }
+  }
 
+  //Borrar elementos de la barra
+  borrar.addEventListener("click", () => {
+    const barra = document.getElementById("barra");
+    barra.innerText = "";
+    numero1=0;
+    operador ='';
+  });
+  //Borrar elementos del historial
+  borrarH.addEventListener("click",()=>{
+      const historial = document.getElementById('historial');
+      historial.innerText = "";
+  });
+
+  //Función para la calculadora
+  function calcular(numero1, numero2, operador, barra) {
+    const historial = document.getElementById("historial");
+    let result;
+    switch (operador) {
+      case "+":
+        result = parseInt(numero1) + parseInt(numero2);
+        barra.innerText = result;
+        historial.innerHTML += `<li>${numero1} + ${numero2}  ${result}</li>`;
+        break;
+      case "-":
+        result = parseInt(numero1) - parseInt(numero2);
+        barra.innerText = result;
+        historial.innerHTML += `<li>${numero1} - ${numero2}  ${result}</li>`;
+        break;
+      case "*":
+        result = parseInt(numero1) * parseInt(numero2);
+        barra.innerText = result;
+        historial.innerHTML += `<li>${numero1} * ${numero2}  ${result}</li>`;
+        break;
+      case "/":
+        if (parseInt(numero2) == 0) {
+          historial.innerHTML += `<li>División en 0</li>`;
+        } else {
+          result = parseInt(numero1) / parseInt(numero2);
+          barra.innerText = result;
+          historial.innerHTML += `<li>${numero1} / ${numero2}  ${result}</li>`;
+        }
+        break;
+      default:
+        historial.innerHTML += `<li>0</li>`;
+        break;
+    }
+  }
+};
+
+//Hacer texto
+function hacerTexto(texto) {
+  barra.innerText += texto;
+}
+//Tocar el evento
 for (let i = 0; i < tecla.length; i++) {
-    tecla[i].addEventListener('click', teclado)
+  tecla[i].addEventListener("click", teclado);
 }
-//Operar la calculadora
-const historial = document.getElementById('historial')
-const operar = (barra, operador) => {
-    const valores = barra.split(operador)
-    switch(operador){
-        case '+':
-            const suma =parseInt(valores[0])+parseInt(valores[1])
-            historial.innerHTML += `<li> ${barra}   ${suma} </li>`
-            break;
-        case '-':
-            const resta =parseInt(valores[0])-parseInt(valores[1])
-            historial.innerHTML += `<li> ${barra}   ${resta} </li>`
-            break;
-        case 'x':
-            const multi =parseInt(valores[0])*parseInt(valores[1])
-            historial.innerHTML += `<li> ${barra}   ${multi} </li>`
-            break;
-        case '/':
-            const divi =parseInt(valores[0])/parseInt(valores[1])
-            historial.innerHTML += `<li> ${barra}   ${divi} </li>`
-            break;
-
-    }
-}
-
-// const historial = document.getElementById('historial')
-// const operar = (barra, operador) => {
-//     const valores = barra.split(operador)
-//     switch(operador){
-//         case '+':
-//             const suma =parseInt(valores[0])+parseInt(valores[1])
-//             historial.innerHTML += `<li> ${barra}   ${suma} </li>`
-//             break;
-//         case '-':
-//             const resta =parseInt(valores[0])-parseInt(valores[1])
-//             historial.innerHTML += `<li> ${barra}   ${resta} </li>`
-//             break;
-//         case 'x':
-//             const multi =parseInt(valores[0])*parseInt(valores[1])
-//             historial.innerHTML += `<li> ${barra}   ${multi} </li>`
-//             break;
-//         case '/':
-//             const divi =parseInt(valores[0])/parseInt(valores[1])
-//             historial.innerHTML += `<li> ${barra}   ${divi} </li>`
-//             break;
-
-//     }
-// }
